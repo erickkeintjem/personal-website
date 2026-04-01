000ac7213668a3ac3844a3660e19436f8897bf47
erickkeintjem <erickkeintjem19@gmail.com>
2026-04-01 19:52:29 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// typing
+var typed = new Typed("#typing", {
+  strings: [
+    "Undergraduate Student",
+    "Cyber Security Enthusiast",
+    "AI Explorer",
+    "Blockchain Learner",
+    "Gamer & Music Lover"
+  ],
+  typeSpeed: 50,
+  backSpeed: 30,
+  loop: true
+});
+
+// animation
+AOS.init({
+  duration: 1000
+});
